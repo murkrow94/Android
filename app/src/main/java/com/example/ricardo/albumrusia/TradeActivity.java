43c6ee7ab54c2b8077402869994e39831b705be0
Ricardo <r,yorke94@gmail.com>
2018-06-10 15:25:14 -0500

    Cambio de fondo de pantalla y avance

--- app/src/main/java/com/example/ricardo/albumrusia/TradeActivity.java
+++ app/src/main/java/com/example/ricardo/albumrusia/TradeActivity.java
@@ -1,19 +1,37 @@
 package com.example.ricardo.albumrusia;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
-
-public class TradeActivity extends Fragment {
+public class TradeActivity extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.activity_trade, null);
     }
-
 }
